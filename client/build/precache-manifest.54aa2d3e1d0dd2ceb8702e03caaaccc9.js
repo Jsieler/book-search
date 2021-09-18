e753a18873789729b5a9c654a569486b400f69fd
Jsieler <joshuasieler@Joshs-MacBook-Air.local>
2021-09-18 09:13:59 -0600

    mm

---
+++ client/build/precache-manifest.54aa2d3e1d0dd2ceb8702e03caaaccc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63cb36aded050bb12e78c0f528358c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756d2cd01f45eb859d39",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1563722deda69f6e6e1b",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "756d2cd01f45eb859d39",
+    "url": "/static/js/2.364f279e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.364f279e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1563722deda69f6e6e1b",
+    "url": "/static/js/main.947dce38.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
